2fcc326f7e04934004bebe3b59f8e257a41eaffa
Jason Armbrecht <jason.armbrecht@gmail.com>
2025-12-08 10:53:42 +0930

    Fixed admin ui login page and pb schema

--- admin/src/pages/LoginPage.tsx
+++ admin/src/pages/LoginPage.tsx
@@ -26,11 +26,23 @@ export function LoginPage() {
       <form onSubmit={submit}>
         <label>
           Email
-          <input value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            autoComplete="username"
+            required
+          />
         </label>
         <label>
           Password
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            autoComplete="current-password"
+            required
+          />
         </label>
         <button type="submit">Sign in</button>
         {error && <p className="error">{error}</p>}
